d3883ca26920a00a7513d83eef2323822ef6005b
yuhang <yuhang@stargoto.com>
2017-12-07 09:53:59 +0800

    。。。。

--- src/main/java/com/yh/st/web/controller/WorkController.java
+++ src/main/java/com/yh/st/web/controller/WorkController.java
@@ -1,14 +1,23 @@
 package com.yh.st.web.controller;
 
+import java.io.BufferedInputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yh.st.base.controller.BaseController;
+import com.yh.st.base.domain.News;
+import com.yh.st.base.service.NewsService;
 import com.yh.st.base.service.NoticeService;
+import com.yh.st.common.util.poi.CommonExcel;
 
 /**
  * 我的工作台
@@ -23,6 +32,8 @@ public class WorkController extends BaseController {
 
 	@Resource
 	private NoticeService noticeService;
+	@Resource
+	private NewsService newsService;
 
 	/**
 	 * 公告列表
@@ -77,5 +88,42 @@ public class WorkController extends BaseController {
 	@RequestMapping("newsView")
 	public String newsView() {
 		return "work/news-list";
+	}
+
+	// TODO excel导入
+	// @RequestMapping(value = "/importEmployee")
+	// public void uploadExcel(@RequestParam("file") MultipartFile file,
+	// HttpServletRequest request) {
+	// try {
+	// CommonExcel excel = new CommonExcel();
+	// List<Object> employeeDTOList = excel.importExcel(Test.class,
+	// file.getInputStream());
+	// // 可做持久化操作，现只打印观察
+	// for (Object employeeDTO : employeeDTOList) {
+	// System.out.println(((Test) employeeDTO).toString());
+	// }
+	// } catch (Exception e) {
+	// }
+	// }
+
+	@RequestMapping("excelNews")
+	public void excelNews(HttpServletResponse response) {
+		try {
+			response.setContentType("application/xls");
+			response.addHeader("Content-Disposition", "attachment;filename="
+					+ new String(("eeelist").getBytes("UTF-8"), "iso-8859-1") + ".xls");
+			CommonExcel base = new CommonExcel();
+			Map<String, Object> params = new HashMap<String, Object>();
+			BufferedInputStream input = new BufferedInputStream(base.excelModelbyClass(News.class,
+					newsService.queryNews(params, 1, 100).getList()));
+			byte buffBytes[] = new byte[1024];
+			OutputStream os = response.getOutputStream();
+			int read = 0;
+			while ((read = input.read(buffBytes)) != -1) {
+				os.write(buffBytes, 0, read);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
